af56f884a8d05f582fd4837081b298fb700f06a4
ynei <yen.2302700228@st.umt.edu.vn>
2026-03-17 15:00:29 +0700

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,19 +42,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("io.coil-kt:coil-compose:2.6.0")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    val cameraxVersion = "1.3.0"
-    implementation("androidx.camera:camera-core:$cameraxVersion")
-    implementation("androidx.camera:camera-camera2:$cameraxVersion")
-    implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
-    implementation("androidx.camera:camera-view:$cameraxVersion")
-
-    // Hiển thị ảnh (Coil)
-    implementation("io.coil-kt:coil:2.5.0")
-
-    // Thư viện chọn ảnh nhanh (Image Picker)
-    implementation("com.github.dhaval2404:imagepicker:2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
