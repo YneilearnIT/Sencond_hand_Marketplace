b10f4fdc7e67f272f57b3fbd1c651ab2c567ffd9
ynei <yen.2302700228@st.umt.edu.vn>
2026-03-23 03:11:35 +0700

    Done UI App

--- app/src/main/java/com/yen/sencond_handmarketplace/PromotionDetailActivity.kt
+++ app/src/main/java/com/yen/sencond_handmarketplace/PromotionDetailActivity.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ChatActivity : AppCompatActivity() {
+class PromotionDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_chat)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.headerChat)) { v, insets ->
+        setContentView(R.layout.activity_promotion_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.headerPush)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
