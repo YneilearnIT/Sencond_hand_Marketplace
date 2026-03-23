2f0d4eeff9d329ce1aff2e2b5285f114c3009a69
theesanh <anhnguyenck3@gmail.com>
2026-03-23 23:48:36 +0700

    fix: sửa lỗi kết nối database và cập nhật script kiểm tra db

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: '127.0.0.1',
-    database: 'shm_market',
+    database: 'postgres',
     password: '2005',
     port: 5432,
 });
